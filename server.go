9f79c007cf8a47cc9687685841f597ad830d7489
Cole Kennedy <colek42@gmail.com>
2018-07-27 18:33:06 -0400

    Get JWT

--- server.go
+++ server.go
@@ -1,21 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"html"
+	"errors"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/caleblloyd/okta-jwt-verifier-golang"
 )
 
 type config struct {
-	client_id   string
-	issuer      string
-	tokenName   string
-	redirectURL string
+	clientID       string
+	clientSecret   string
+	issuer         string
+	tokenName      string
+	redirectURL    string
+	listenPort     string
+	listenAddress  string
+	oktaURL        string //Make sure this is https
+	requestTimeout time.Duration
 }
 
 var conf config
@@ -33,8 +39,8 @@ func removeSockIfExists() {
 }
 
 func init() {
-	client_id := os.Getenv("CLIENT_ID")
-	if client_id == "" {
+	clientID := os.Getenv("CLIENT_ID")
+	if clientID == "" {
 		log.Fatalln("Must specify CLIENT_ID env variable - These can be found on the 'General' tab of the Web application that you created earlier in the Okta Developer Console.")
 	}
 
@@ -43,33 +49,29 @@ func init() {
 		log.Fatalln("This is the URL of the authorization server that will perform authentication. All Developer Accounts have a 'default' authorization server. The issuer is a combination of your Org URL (found in the upper right of the console home page) and /oauth2/default. For example, https://dev-1234.oktapreview.com/oauth2/default.")
 	}
 
-	conf := config{
-		client_id: client_id,
-		issuer:    issuer,
+	conf = config{
+		clientID: clientID,
+		issuer:   issuer,
 	}
 }
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", validateCookieHandler)
+	// cookie validation
+	// if no cookie, redirect to okta
+	// if cookie, valiate jwt with https://github.com/caleblloyd/okta-jwt-verifier-golang
+	// if jwt valid return 200
+	// if jwt not valide, return 403 with "X-Authorize-Redirect" with the Okta Authorize URL
+	//log.Println(html.EscapeString(r.URL.Path))
+	//fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	//})
 
-		// cookie validation
-		// if no cookie, redirect to okta
-		// if cookie, valiate jwt with https://github.com/caleblloyd/okta-jwt-verifier-golang
-		// if jwt valid return 200
-		// if jwt not valide, return 403 with "X-Authorize-Redirect" with the Okta Authorize URL
-		log.Println(html.EscapeString(r.URL.Path))
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
-
-	http.HandleFunc("/sso/authorization-code/callback", func(w http.ResponseWriter, r *http.Request) {
-		// read the auth code from URL Param
-		// call Okta to exchange Auth Code for JWT
-		// set JWT in Cookie
-		// if error, redirect to "/sso/error?error={error string}"
-		log.Println(html.EscapeString(r.URL.Path))
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	http.HandleFunc("/sso/authorization-code/callback", callbackHandler)
+	// read the auth code from URL Param
+	// call Okta to exchange Auth Code for JWT
+	// set JWT in Cookie
+	// if error, redirect to "/sso/error?error={error string}"
 
 	removeSockIfExists()
 	unixListener, err := net.Listen("unix", sock)
@@ -89,7 +91,7 @@ func validateCookieHandler(w http.ResponseWriter, r *http.Request) {
 	tokenCookie, err := r.Cookie(conf.tokenName)
 	switch {
 	case err == http.ErrNoCookie:
-		log.Println("No cookie, redirecting to %s for auth", conf.redirectURL)
+		log.Printf("No cookie, redirecting to %s for auth", conf.redirectURL)
 		http.Redirect(w, r, conf.redirectURL, http.StatusPermanentRedirect)
 		return
 	case err != nil:
@@ -106,6 +108,61 @@ func validateCookieHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	verifier := jwtverifierSetup.New()
-	token, err := verifier.VerifyAccessToken(tokenCookie.String())
+	_, err = verifier.VerifyAccessToken(tokenCookie.String())
 
+	if err != nil {
+		w.Header().Set("X-Authorize-Redirect", conf.redirectURL)
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	if err == nil {
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func callbackHandler(w http.ResponseWriter, r *http.Request) {
+	//Read auth code from URL Param
+	params := r.URL.Query()
+	code := params.Get("code")
+
+	//Check for no code to guard against ddos
+	if code == "" {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+}
+
+func getJWT(code string) (string, error) {
+	client := &http.Client{
+		Timeout: time.Second * conf.requestTimeout,
+	}
+	req, err := http.NewRequest("POST", conf.oktaURL, nil)
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.URL.Query().Add("code", code)
+	req.URL.Query().Add("client_id", conf.clientID)
+	req.URL.Query().Add("client_secret", conf.clientSecret)
+	req.URL.Query().Add("grant_type", "authorization_code")
+	req.URL.Query().Add("scope", "openid")
+	req.URL.Query().Add("redirect_uri", conf.redirectURL)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	bodyStr := string(bodyBytes)
+
+	if resp.StatusCode == http.StatusOK {
+		return bodyStr, nil
+	}
+
+	return "", errors.New(bodyStr)
 }
