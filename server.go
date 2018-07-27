6d1216a8ab108151c5c78eb3f1a00078440e62c7
Caleb Lloyd <caleblloyd@gmail.com>
2018-07-27 11:40:55 -0400

    add redirect and error handling

--- server.go
+++ server.go
@@ -24,6 +24,20 @@ func removeSockIfExists() {
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// cookie validation
+		// if no cookie, redirect to okta
+		// if cookie, valiate jwt with https://github.com/caleblloyd/okta-jwt-verifier-golang
+		// if jwt valid return 200
+		// if jwt not valide, return 403 with "X-Authorize-Redirect" with the Okta Authorize URL
+		log.Println(html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/sso/authorization-code/callback", func(w http.ResponseWriter, r *http.Request) {
+		// read the auth code from URL Param
+		// call Okta to exchange Auth Code for JWT
+		// set JWT in Cookie
+		// if error, return 403 with "X-Authorize-Error" with error Param
 		log.Println(html.EscapeString(r.URL.Path))
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
