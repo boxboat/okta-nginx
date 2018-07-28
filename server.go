debe7684b5055d24b611d8ea7fdfd355416f63e6
Cole Kennedy <colek42@gmail.com>
2018-07-28 15:48:56 -0400

    refactor to remove globals and init

--- server.go
+++ server.go
@@ -10,40 +10,33 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
 	jwtverifier "github.com/caleblloyd/okta-jwt-verifier-golang"
 )
 
-type config struct {
-	clientID         string
-	clientSecret     string
-	issuer           string
-	tokenName        string
-	loginRedirectURL string
-	listenPort       string
-	listenAddress    string
-	oktaLoginBaseURL string
-	requestTimeout   time.Duration
-}
-
-var conf config
-var verifier *jwtverifier.JwtVerifier
-
 const sock = "/var/run/auth.sock"
+const tokenName = "okta-jwt"
 
-func removeSockIfExists() {
-	_, err := os.Stat(sock)
-	if err == nil {
-		err = os.Remove(sock)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
+type config struct {
+	clientID         string        //CLIENT_ID
+	clientSecret     string        //CLIENT_SECRET
+	issuer           string        //ISSUER
+	loginRedirectURL string        //LOGIN_REDIRECT_URL
+	tokenName        string        //constant
+	oktaLoginBaseURL string        //computed
+	requestTimeout   time.Duration //Default of 5 seconds if no env set
+	verifier         *jwtverifier.JwtVerifier
 }
 
-func init() {
+type jwtResponse struct {
+	AccessToken string `json:"access_token"`
+}
+
+func getConfig() config {
+	//Populate config from env vars
 	clientID := os.Getenv("CLIENT_ID")
 	if clientID == "" {
 		log.Fatalln("Must specify CLIENT_ID env variable - These can be found on the 'General' tab of the Web application that you created earlier in the Okta Developer Console.")
@@ -64,13 +57,15 @@ func init() {
 		log.Fatalln("Must specify LOGIN_REDIRECT_URL env variable - These can be found on the 'General' tab of the Web application that you created earlier in the Okta Developer Console.")
 	}
 
-	oktaLoginBaseURL := issuer + "/v1/authorize" +
-		"?client_id=" + url.QueryEscape(clientID) +
-		"&redirect_uri=" + url.QueryEscape(loginRedirectURL) +
-		"&response_type=code" +
-		"&scope=openid" +
-		"&nonce=123"
+	requestTimeoutInt, err := strconv.Atoi(os.Getenv("REQUEST_TIMEOUT"))
+	requestTimeOut := time.Duration(5)
+	//Just youse 5 seconds if there is an error parsing
+	if err == nil {
+		log.Println("Unable to parse REQUEST_TIMEOUT environment variable, using a default of 5 seconds")
+		requestTimeOut = time.Duration(requestTimeoutInt)
+	}
 
+	//Initialize validator
 	toValidate := map[string]string{}
 	toValidate["aud"] = "api://default"
 	toValidate["cid"] = clientID
@@ -79,36 +74,43 @@ func init() {
 		Issuer:           issuer,
 		ClaimsToValidate: toValidate,
 	}
-	verifier = jwtverifierSetup.New()
 
-	conf = config{
+	oktaLoginBaseURL := issuer + "/v1/authorize" +
+		"?client_id=" + url.QueryEscape(clientID) +
+		"&redirect_uri=" + url.QueryEscape(loginRedirectURL) +
+		"&response_type=code" +
+		"&scope=openid" +
+		"&nonce=123"
+
+	return config{
 		clientID:         clientID,
 		clientSecret:     clientSecret,
 		loginRedirectURL: loginRedirectURL,
 		issuer:           issuer,
+		tokenName:        tokenName,
+		requestTimeout:   requestTimeOut,
+		verifier:         jwtverifierSetup.New(),
 		oktaLoginBaseURL: oktaLoginBaseURL,
-		tokenName:        "okta-jwt",
 	}
 }
 
 func main() {
+	runServer(getConfig())
+}
 
-	http.HandleFunc("/", validateCookieHandler)
-	// cookie validation
-	// if no cookie, redirect to okta
-	// if cookie, valiate jwt with https://github.com/caleblloyd/okta-jwt-verifier-golang
-	// if jwt valid return 200
-	// if jwt not valide, return 401 with "X-Authorize-Redirect" with the Okta Authorize URL
-	//log.Println(html.EscapeString(r.URL.Path))
-	//fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	//})
+func runServer(conf config) {
 
-	http.HandleFunc("/sso/authorization-code/callback", callbackHandler)
-	// read the auth code from URL Param
-	// call Okta to exchange Auth Code for JWT
-	// set JWT in Cookie
-	// if error, redirect to "/sso/error?error={error string}"
+	//Validate cookie on each request
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		validateCookieHandler(w, r, conf)
+	})
 
+	//Authorization code callback
+	http.HandleFunc("/sso/authorization-code/callback", func(w http.ResponseWriter, r *http.Request) {
+		callbackHandler(w, r, conf)
+	})
+
+	//Listen on unix socket instead of http
 	removeSockIfExists()
 	unixListener, err := net.Listen("unix", sock)
 	if err != nil {
@@ -116,18 +118,24 @@ func main() {
 	}
 	defer removeSockIfExists()
 
-	if err = os.Chmod(sock, 0777); err != nil {
+	//might need to change owner of the socket
+	if err = os.Chmod(sock, 0660); err != nil {
 		log.Fatal(err)
 	}
 
-	http.Serve(unixListener, nil)
+	err = http.Serve(unixListener, nil)
+	if err != nil {
+		log.Fatalf("Error serving on socket, err: %v", err)
+	}
 }
 
-func validateCookieHandler(w http.ResponseWriter, r *http.Request) {
+func validateCookieHandler(w http.ResponseWriter, r *http.Request, conf config) {
+	redirectURL := conf.oktaLoginBaseURL + "&state=" + url.QueryEscape(r.URL.RequestURI())
+
 	tokenCookie, err := r.Cookie(conf.tokenName)
 	switch {
 	case err == http.ErrNoCookie:
-		w.Header().Set("X-Authorize-Redirect", conf.oktaLoginBaseURL+"&state="+url.QueryEscape(r.URL.RequestURI()))
+		w.Header().Set("X-Authorize-Redirect", redirectURL)
 		w.WriteHeader(http.StatusUnauthorized)
 		log.Printf("No Cookie")
 		log.Printf("X-Authorize-Redirect,  %v", w.Header().Get("X-Authorize-Redirect"))
@@ -138,10 +146,10 @@ func validateCookieHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, err = verifier.VerifyAccessToken(tokenCookie.Value)
+	_, err = conf.verifier.VerifyAccessToken(tokenCookie.Value)
 
 	if err != nil {
-		w.Header().Set("X-Authorize-Redirect", conf.oktaLoginBaseURL+"&state="+url.QueryEscape(r.URL.RequestURI()))
+		w.Header().Set("X-Authorize-Redirect", redirectURL)
 		w.WriteHeader(http.StatusUnauthorized)
 		log.Printf("JWT Validation Error,  %v", err)
 		log.Printf("X-Authorize-Redirect,  %v", w.Header().Get("X-Authorize-Redirect"))
@@ -153,7 +161,8 @@ func validateCookieHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func callbackHandler(w http.ResponseWriter, r *http.Request) {
+func callbackHandler(w http.ResponseWriter, r *http.Request, conf config) {
+
 	//Read auth code from URL Param
 	params := r.URL.Query()
 	code := params.Get("code")
@@ -164,19 +173,21 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	jwt, err := getJWT(code)
+	//Redirtect if error
+	jwt, err := getJWT(code, conf)
 	if err != nil {
 		http.Redirect(w, r, "/sso/error?error="+url.QueryEscape(err.Error()), http.StatusTemporaryRedirect)
 		return
 	}
 
-	_, err = verifier.VerifyAccessToken(jwt)
+	_, err = conf.verifier.VerifyAccessToken(jwt)
 	if err != nil {
 		log.Printf("JWT Validation Error,  %v", err)
 		http.Redirect(w, r, "/sso/error?error="+url.QueryEscape(err.Error()), http.StatusTemporaryRedirect)
 		return
 	}
 
+	//Set cokkie if code valid
 	cookie := &http.Cookie{
 		Name:     conf.tokenName,
 		Value:    jwt,
@@ -185,6 +196,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	http.SetCookie(w, cookie)
 
+	//Redirect to requested page
 	state := params.Get("state")
 	if state == "" {
 		state = "/"
@@ -193,21 +205,23 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, state, http.StatusTemporaryRedirect)
 }
 
-type jwtResponse struct {
-	AccessToken string `json:"access_token"`
-}
-
-func getJWT(code string) (string, error) {
+//getJWT queries the okta server with an access code.  A valid request will return a JWT access token.
+func getJWT(code string, conf config) (string, error) {
 	client := &http.Client{
 		Timeout: time.Second * conf.requestTimeout,
 	}
+
 	reqBody := []byte("code=" + url.QueryEscape(code) +
 		"&client_id=" + url.QueryEscape(conf.clientID) +
 		"&client_secret=" + url.QueryEscape(conf.clientSecret) +
 		"&redirect_uri=" + url.QueryEscape(conf.loginRedirectURL) +
 		"&grant_type=authorization_code" +
 		"&scope=openid")
+
 	req, err := http.NewRequest("POST", conf.issuer+"/v1/token", bytes.NewBuffer(reqBody))
+	if err != nil {
+		return "", err
+	}
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
@@ -222,6 +236,7 @@ func getJWT(code string) (string, error) {
 		return "", err
 	}
 
+	//200 == authorization suceeded.
 	if resp.StatusCode == http.StatusOK {
 		jsonResponse := &jwtResponse{}
 		err = json.Unmarshal(bodyBytes, &jsonResponse)
@@ -233,4 +248,14 @@ func getJWT(code string) (string, error) {
 
 	bodyStr := string(bodyBytes)
 	return "", errors.New(bodyStr)
+}
+
+func removeSockIfExists() {
+	_, err := os.Stat(sock)
+	if err == nil {
+		err = os.Remove(sock)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
