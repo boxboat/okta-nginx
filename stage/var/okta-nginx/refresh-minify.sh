0483e81a4db9b77a7fed572cb7b4d79dd6b19e0d
Caleb Lloyd <caleblloyd@gmail.com>
2021-02-16 21:58:30 -0500

    minor fixes

--- stage/var/okta-nginx/refresh-minify.sh
+++ stage/var/okta-nginx/refresh-minify.sh
@@ -4,11 +4,11 @@
 
 cd $(dirname $0)
 
-echo '<script type="text/javascript">'$(\
+echo '<script type="text/javascript">'"$(\
     sed -r 's/console\.log\([^\)]+\);?//g' refresh.js \
     | sed -r 's|///.*||g' \
     | sed ':a;N;$!ba;s/\n/ /g' \
     | sed -r 's/\s+/ /g' \
     | sed -r "s|/sso/|${1}|g" \
     | sed -r "s/'/\\\'/g"
-)'</script>'
+)"'</script>'
